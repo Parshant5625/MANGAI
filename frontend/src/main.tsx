c379b65a561df7fc2071c06d20c050b1b8b0279b
Parshant5625 <parshantc82@gmail.com>
2026-09-11 21:38:42 +0530

    Load final command center styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./phase12-polish.css";
+import "./final-command-center.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
